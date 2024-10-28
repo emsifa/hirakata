6e5ef673fdcf6125ce498f7f6809944530215055
Muhammad Syifa <emsifa@gmail.com>
2024-10-28 15:08:52 +0700

    add Google Analytics tracking code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
+	import { PUBLIC_GTAG_ID } from '$env/static/public';
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	{#if PUBLIC_GTAG_ID}
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-{PUBLIC_GTAG_ID}"></script>
+		<script data-gtag={PUBLIC_GTAG_ID}>
+			const gtagId = document.currentScript.dataset.gtag;
+			window.dataLayer = window.dataLayer || [];
+			function gtag() {
+				dataLayer.push(arguments);
+			}
+			gtag('js', new Date());
+			gtag('config', gtagId);
+		</script>
+	{/if}
+</svelte:head>
 
 {@render children()}
