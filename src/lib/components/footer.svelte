cf7983c3e22878864e86dbde72924e358c8560b5
Muhammad Syifa <emsifa@gmail.com>
2024-11-02 11:09:46 +0700

    improve footer style

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,16 +1,33 @@
 <div class="text-center">
-	<div class="flex flex-wrap justify-between px-4">
-		<p class="text-sm text-gray-400 opacity-75">
+	<div class="flex flex-col-reverse flex-wrap items-center justify-between md:flex-row">
+		<p class="mt-2 text-sm text-gray-400 opacity-75 md:mt-0">
 			© 2024 &mdash; <a
 				href="https://github.com/emsifa"
 				class="hover:text-primary-500 hover:underline">Muhammad Syifa</a
 			>
 		</p>
-		<div class="flex flex-wrap space-x-3 text-sm">
-			<a href="/hiragana" class="text-gray-400 hover:text-primary-400">Hiragana-chan</a>
-			<a href="/katakana" class="text-gray-400 hover:text-primary-400">Katakana-kun</a>
-			<span>|</span>
-			<a href="https://trakteer.id/emsifa" class="text-rose-400">Trakteer</a>
+		<div class="flex flex-wrap items-center gap-3 text-sm">
+			<a href="/" aria-label="Home" class="text-gray-400">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="h-4 w-4"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+					/>
+				</svg>
+			</a>
+			<span class="text-gray-600">|</span>
+			<a href="/hiragana" class="text-gray-400 hover:text-primary-400">Hiragana</a>
+			<a href="/katakana" class="text-gray-400 hover:text-primary-400">Katakana</a>
+			<span class="text-gray-600">|</span>
+			<a href="https://trakteer.id/emsifa" class="text-rose-400"> Trakteer </a>
 		</div>
 	</div>
 </div>
